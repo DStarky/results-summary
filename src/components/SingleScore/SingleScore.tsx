6570e20ec0ed9ba7614fed333656ff1d90c19e59
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-11-07 07:11:42 +0500

    added color visualisation for summary

--- src/components/SingleScore/SingleScore.tsx
+++ src/components/SingleScore/SingleScore.tsx
@@ -9,13 +9,22 @@ interface SingleScoreProps {
 
 const SingleScore = ({ category, score, icon, color }: SingleScoreProps) => {
 	return (
-		<div className={styles.root} data-color={color}>
+		<div
+			className={styles.root}
+			data-color={color}
+			data-width={`${score}px`}>
+			<div
+				className={styles.progress}
+				style={{ width: `${score}%` }}
+				data-color={color}></div>
 			<img
 				src={icon}
 				alt=''
 			/>
 			<p className={styles.category}>{category}</p>
-			<p className={styles.score}><span>{score}</span> / 100</p>
+			<p className={styles.score}>
+				<span>{score}</span> / 100
+			</p>
 		</div>
 	);
 };
